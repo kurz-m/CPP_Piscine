b855704f559d71720c94e8985b843c90178fb605
makurz <makurz@student.42heilbronn.de>
2023-10-17 22:24:19 +0200

    add ex01 from cpp02

--- CPP_Module_02/ex01/Fixed.cpp
+++ CPP_Module_02/ex01/Fixed.cpp
@@ -5,6 +5,16 @@ Fixed::Fixed() : fixed_point_(0)
   std::cout << "Default constructor called" << std::endl;
 }
 
+Fixed::Fixed(const int i) : fixed_point_(i << fractal_bits_)
+{
+  std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float f) : fixed_point_(roundf(f * (1 << fractal_bits_)))
+{
+  std::cout << "Float constructor called" << std::endl;
+}
+
 Fixed::Fixed(const Fixed& fixed)
 {
   std::cout << "Copy constructor called" << std::endl;
@@ -27,12 +37,26 @@ Fixed::~Fixed()
 
 int Fixed::get_raw_bits() const
 {
-  std::cout << "get_raw_bits member function called" << std::endl;
   return fixed_point_;
 }
 
 void  Fixed::set_raw_bits(const int raw)
 {
-  std::cout << "set_raw_bits member function called" << std::endl;
   fixed_point_ = raw;
+}
+
+float Fixed::to_float() const
+{
+  return (float)fixed_point_ / (float)(1 << fractal_bits_);
+}
+
+int Fixed::to_int() const
+{
+  return fixed_point_ >> fractal_bits_;
+}
+
+std::ostream& operator<<(std::ostream& o, const Fixed& rhs)
+{
+  o << rhs.to_float();
+  return o;
 }
