5f8134040a13cf34eb04e7fc1fb5b453021a6eff
makurz <makurz@student.42heilbronn.de>
2023-10-18 18:28:52 +0200

    start of ex02

---
+++ CPP_Module_02/ex02/Fixed.hpp
@@ -0,0 +1,29 @@
+#ifndef CLASS_FIXED_HPP_
+# define CLASS_FIXED_HPP_
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+class Fixed {
+public:
+  Fixed();
+  Fixed(const int);
+  Fixed(const float);
+  Fixed(const Fixed&);
+  Fixed& operator=(const Fixed&);
+  ~Fixed();
+
+  int get_raw_bits() const;
+  void  set_raw_bits(const int raw);
+  float to_float() const;
+  int to_int() const;
+
+private:
+  int fixed_point_;
+  static const int  fractal_bits_ = 8;
+};
+
+std::ostream& operator<<(std::ostream&, const Fixed& rhs);
+
+#endif
