1083a58f30d1aefe8544f53976393b338228b60e
Markus Kurz <85294588+kurz-m@users.noreply.github.com>
2023-11-30 15:14:30 +0000

    finish cpp03/ex01

--- CPP_Module_03/ex01/main.cpp
+++ CPP_Module_03/ex01/main.cpp
@@ -1,30 +1,45 @@
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 int main()
 {
-  ClapTrap trap = ClapTrap("Herbert");
+  {
+    ScavTrap part = ScavTrap("Dieter");
 
-  trap.attack("Brunhilde");
-  trap.take_damage(3);
-  trap.be_repaired(1);
-  trap.be_repaired(2);
+    part.attack("HP");
+    part.take_damage(30);
+    part.be_repaired(4);
+    part.attack("Forest");
+    part.guard_gate();
+    part.take_damage(74);
+    part.attack("Nothing");
+  }
 
-  trap.attack("Thor");
-  trap.take_damage(3);
-  trap.be_repaired(1);
-  trap.be_repaired(2);
+  {
+    ClapTrap trap = ClapTrap("Herbert");
+    trap.attack("Brunhilde");
+    trap.take_damage(3);
+    trap.be_repaired(1);
+    trap.be_repaired(2);
 
-  trap.attack("Backdoor");
-  trap.take_damage(3);
-  trap.be_repaired(1);
-  trap.be_repaired(2);
+    trap.attack("Thor");
+    trap.take_damage(3);
+    trap.be_repaired(1);
+    trap.be_repaired(2);
 
-  trap.attack("Trapdoor");
-  trap.take_damage(3);
-  trap.be_repaired(1);
-  trap.be_repaired(2);
+    trap.attack("Backdoor");
+    trap.take_damage(3);
+    trap.be_repaired(1);
+    trap.be_repaired(2);
 
-  trap.take_damage(8);
+    trap.attack("Trapdoor");
+    trap.take_damage(3);
+    trap.be_repaired(1);
+    trap.be_repaired(2);
+
+    trap.take_damage(8);
+}
+
+
 
   return EXIT_SUCCESS;
 }
