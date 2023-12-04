b461dee2748e40993b4d7da6c6da0750c4f42005
kurz-m <m.kurz92@gmail.com>
2023-12-04 23:14:39 +0100

    update main according to subject

--- CPP_Module_04/ex01/main.cpp
+++ CPP_Module_04/ex01/main.cpp
@@ -1,17 +1,38 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
 
+// int main()
+// {
+//   Dog dog;
+//   Dog dog2;
+// 
+//   dog.create_idea("Nothing");
+//   dog.create_idea("Eat bone");
+//   dog.list_ideas();
+//   dog.print_addr();
+//   dog2 = dog;
+//   dog2.print_addr();
+//   dog2.list_ideas();
+//   return EXIT_SUCCESS;
+// }
+
 int main()
 {
-  Dog dog;
-  Dog dog2;
+  Animal* horde[10];
+  int i = 0;
 
-  dog.create_idea("Nothing");
-  dog.create_idea("Eat bone");
-  dog.list_ideas();
-  dog.print_addr();
-  dog2 = dog;
-  dog2.print_addr();
-  dog2.list_ideas();
+  while (i < 5) {
+    horde[i++] = new Dog;
+  }
+
+  while (i < 10) {
+    horde[i++] = new Cat;
+  }
+  i = 0;
+  while (i < 10) {
+    horde[i]->make_sound();
+    delete horde[i++];
+  }
+
   return EXIT_SUCCESS;
 }
