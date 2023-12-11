a900e18299283ab881e2baa01e51f9772f0e686b
kurz-m <m.kurz92@gmail.com>
2023-12-11 20:41:57 +0100

    update utils colorcode

--- CPP_Module_03/ex00/utils.hpp
+++ CPP_Module_03/ex00/utils.hpp
@@ -4,12 +4,12 @@
 #include <iostream>
 
 #define RESET "\033[0m"
-#define RED "\033[0;31m"
-#define YELLOW "\033[0;33m"  // Brain
-#define BLUE "\033[0;34m"   // Animal
-#define MAGENTA "\033[0;35m"// Cat
-#define CYAN "\033[0;36m"   // Dog
-#define GREEN "\033[32m"
+#define RED "\033[1;31m"
+#define YELLOW "\033[1;33m"  // Brain
+#define BLUE "\033[1;34m"   // Animal
+#define MAGENTA "\033[1;35m"// Cat
+#define CYAN "\033[1;36m"   // Dog
+#define GREEN "\033[1;32m"
 
 void  debug_print(const std::string& fmt, const std::string& color);
 
