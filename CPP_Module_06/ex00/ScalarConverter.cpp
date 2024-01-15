b2088702af40c14015549c5bba081c8700abdde2
Markus <markus.kurz@r-stahl.com>
2024-01-15 18:10:20 +0100

    Awesome project!

--- CPP_Module_06/ex00/ScalarConverter.cpp
+++ CPP_Module_06/ex00/ScalarConverter.cpp
@@ -1,7 +1,19 @@
 #include "ScalarConverter.hpp"
+#include <cctype>
+#include <sstream>
 
 ScalarConverter::ScalarConverter() 
-  : literal_type_(INVALID),
+  : type_(INVALID),
+    input_("empty"),
+    out_char_(0),
+    out_int_(0),
+    out_float_(0),
+    out_double_(0)
+{}
+
+ScalarConverter::ScalarConverter(const std::string& str) 
+  : type_(INVALID),
+    input_(str),
     out_char_(0),
     out_int_(0),
     out_float_(0),
@@ -20,7 +32,23 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter&)
 
 ScalarConverter::~ScalarConverter() {}
 
-static int  ScalarConverter::convert(const std::string& str)
+void  ScalarConverter::convert(const std::string& str)
 {
-  ScalarConverter container;
+  ScalarConverter container(str);
+}
+
+ScalarConverter::Type  ScalarConverter::get_type()
+{
+  if (input_.length() == 1 && std::isalpha(input_[0]) != 0) {
+    return CHAR;
+  }
+  std::istringstream  inbuf(input_);
+  inbuf >> out_int_;
+  if (inbuf.fail()) {
+
+  }
+  inbuf.clear();
+  inbuf.str(input_);
+
+  return INVALID;
 }
--- CPP_Module_06/ex00/ScalarConverter.hpp
+++ CPP_Module_06/ex00/ScalarConverter.hpp
@@ -2,6 +2,8 @@
 # define SCALARCONVERTER_HPP_
 
 #include <string>
+#include <cstdlib>
+#include <iostream>
 
 class ScalarConverter {
 public:
@@ -14,19 +16,29 @@ public:
     S_DOUBLE,
     INVALID,
   };
-  static int  convert(const std::string&);
+
+  static void convert(const std::string&);
+
   ~ScalarConverter();
 
 private:
   ScalarConverter();
+  ScalarConverter(const std::string&);
   ScalarConverter(const ScalarConverter&);
   ScalarConverter& operator=(const ScalarConverter&);
 
-  Type    literal_type_;
-  char    out_char_;
-  int     out_int_;
-  float   out_float_;
-  double  out_double_;
+  Type  get_type();
+  void  from_char();
+  void  from_int();
+  void  from_float();
+  void  from_double();
+
+  Type        type_;
+  std::string input_;
+  char        out_char_;
+  int         out_int_;
+  float       out_float_;
+  double      out_double_;
 };
 
 #endif
