485478dcc70e6109124c902c213b795d67aa03d7
makurz <makurz@student.42heilbronn.de>
2024-01-16 13:30:42 +0100

    Update testing for cpp05/ex03

--- CPP_Module_05/ex03/main.cpp
+++ CPP_Module_05/ex03/main.cpp
@@ -18,9 +18,60 @@ void  test_president_form()
   delete form;
 }
 
+
+void  test_robotomy_form()
+{
+  log_level("Intern creates robotomy form.", TEST);
+  Intern  task_juggler;
+  AForm*  form;
+  Bureaucrat  manny("Manny", 4);
+
+  form = task_juggler.make_form("robotomy request", "någon");
+  manny.sign_form(*form);
+  manny.execute_form(*form);
+  delete form;
+}
+
+void  test_shrubbery_form()
+{
+  log_level("Intern creates shrubbery form.", TEST);
+  Intern  task_juggler;
+  AForm*  form;
+  Bureaucrat  manny("Manny", 4);
+
+  form = task_juggler.make_form("shrubbery creation", "någon");
+  manny.sign_form(*form);
+  manny.execute_form(*form);
+  delete form;
+}
+
+void  test_non_existing_form()
+{
+  log_level("Intern tries to create non-existing form.", TEST);
+  Intern  task_juggler;
+  AForm*  form;
+  Bureaucrat  manny("Manny", 4);
+
+  try {
+    form = task_juggler.make_form("do not know", "någon");
+    manny.sign_form(*form);
+    manny.execute_form(*form);
+    delete form;
+  }
+  catch (const std::invalid_argument& e) {
+    log_level(std::string("Intern says: ") + e.what(), ERROR);
+  }
+}
+
 int main()
 {
   test_president_form();
+
+  test_robotomy_form();
+
+  test_shrubbery_form();
+
+  test_non_existing_form();
 
   return EXIT_SUCCESS;
 }
