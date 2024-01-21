9327d3185e8e89eb88dc70142b2d0cc23a36af67
makurz <makurz@student.42heilbronn.de>
2024-01-21 23:50:45 +0100

    implement test for vector

--- CPP_Module_08/ex00/main.cpp
+++ CPP_Module_08/ex00/main.cpp
@@ -1,6 +1,47 @@
 #include "easyfind.hpp"
+#include <vector>
+#include <iostream>
+#include <string>
+
+void  print_test_name(const std::string& test_name)
+{
+  std::cout << "[TEST] " << test_name << "\n";
+}
+
+void  test_find_vector()
+{
+  print_test_name("Find number in vector.");
+  int nums[] = {8, 4, 5, 9};
+  std::vector<int>::iterator  result;
+  std::vector<int> v(nums, nums + sizeof(nums) / sizeof(nums[0]));
+
+  result = easyfind(v, 5);
+  int pos = std::distance(v.begin(), result);
+
+  std::cout << "Number " << *result
+            << " was found at position " << pos
+            << " of the vector v.\n\n";
+}
+
+void  test_not_found()
+{
+  print_test_name("Number not found in vector.");
+  int nums[] = {8, 4, 5, 9};
+  std::vector<int>::iterator  result;
+  std::vector<int> v(nums, nums + sizeof(nums) / sizeof(nums[0]));
+
+  try {
+    result = easyfind(v, 42);
+  }
+  catch (std::exception& e) {
+    std::cerr << "Error: " << e.what() << "\n\n";
+  }
+}
 
 int main()
 {
+  test_find_vector();
+
+  test_not_found();
   return EXIT_SUCCESS;
 }
