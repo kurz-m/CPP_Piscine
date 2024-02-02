61415752e61adc9de736781dc2cc3c05d1f2603b
kurz-m <m.kurz92@gmail.com>
2024-02-02 09:07:15 +0100

    add std::vector test to mutantstack

--- CPP_Module_08/ex02/main.cpp
+++ CPP_Module_08/ex02/main.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <list>
+#include <vector>
 
 void  print_test_name(const std::string& test_name)
 {
@@ -21,7 +22,7 @@ MutantStack<T, Container>  create_random_stack(size_t size)
 
 void  test_creating_rnd_stack()
 {
-  print_test_name("random stack generation.");
+  print_test_name("random stack generation with std::deque.");
   MutantStack<int> stack = create_random_stack< int, std::deque<int> >(10);
 
   std::cout << stack;
@@ -40,10 +41,27 @@ void  test_list_container()
   >
   stack = create_random_stack< int, std::list<int> >(10);
 
-  std::cout << "Top of stack: " << stack.top() << "\n";
+  std::cout << stack;
   std::cout << "pop one element\n";
   stack.pop();
-  std::cout << "Top of stack: " << stack.top() << "\n";
+  std::cout << stack;
+  std::cout << std::endl;
+}
+
+void  test_vector_container()
+{
+  print_test_name("take vector container");
+  MutantStack<
+    int,
+    std::vector<int>
+  >
+  stack = create_random_stack< int, std::vector<int> >(10);
+
+  std::cout << stack;
+  std::cout << "pop one element\n";
+  stack.pop();
+  std::cout << stack;
+  std::cout << std::endl;
 }
 
 int main()
@@ -51,6 +69,8 @@ int main()
   test_creating_rnd_stack();
 
   test_list_container();
+
+  test_vector_container();
 
   return EXIT_SUCCESS;
 }
