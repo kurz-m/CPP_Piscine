e5eb9a9a361f9b3ccbe7699cbf921ab2040b100b
makurz <makurz@student.42heilbronn.de>
2024-02-03 17:10:34 +0100

    start date class all over again

--- CPP_Module_09/ex00/Date.hpp
+++ CPP_Module_09/ex00/Date.hpp
@@ -8,17 +8,20 @@ typedef unsigned int uint;
 
 class Date {
 public:
-  Date();
   explicit Date(uint, uint,uint);
   explicit Date(std::string&);
   Date(const Date&);
   Date& operator=(const Date&);
   ~Date();
 
-  bool is_leap_year() const;
-  bool is_valid() const;
   bool operator<(const Date&) const;
   std::istream& operator>>(std::istream&);
+
+private:
+  Date();
+
+  bool is_leap_year() const;
+  bool is_valid() const;
 
   uint  year_;
   uint  month_;
