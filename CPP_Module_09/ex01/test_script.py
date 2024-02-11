b1c2da45693ede7c162d0cf5b523de8b3c57b36f
kurz-m <m.kurz92@gmail.com>
2024-02-11 23:24:07 +0100

    add python testscript to cpp09/01

---
+++ CPP_Module_09/ex01/test_script.py
@@ -0,0 +1,86 @@
+import subprocess
+import os
+
+PROGRAM = './RPN'
+MAKE_TARGETS = ['re', 'fclean']
+
+# Build the program
+if not os.system('make re') == 0:
+    print(f"Error building {PROGRAM}.")
+    exit(1)
+
+# Define test cases
+TESTS = [
+    "2 3 +",
+    "5 7 -",
+    "4 6 *",
+    "8 2 /",
+    "1 2 + 3 *",
+    "9 3 / 2 *",
+    "5 2 / 3 +",
+    "0 9 - 2 *",
+    "8 4 / 2 -",
+    "2 3 4 * + 5 /",
+    "1 2 3 * 4 / +",
+    "7 3 * 4 2 / -",
+    "8 4 / 2 * 6 +",
+    "9 2 / 4 * 6 2 / +",
+    "5 6 * 2 / 7 +",
+    "1 2 + 3 4 * /",
+    "4 2 / 3 * 5 -",
+    "6 2 * 8 3 / + 3 *",
+    "9 4 / 2 * 7 3 / + 6 *",
+]
+
+EXPECTED = [
+    "5",
+    "-2",
+    "24",
+    "4",
+    "9",
+    "6",
+    "5.5",
+    "-18",
+    "0",
+    "2.8",
+    "2.5",
+    "19",
+    "10",
+    "21",
+    "22",
+    "0.25",
+    "1",
+    "44",
+    "41",
+]
+
+# Run simple tests
+for TEST, EXPECT in zip(TESTS, EXPECTED):
+    print(f"Running test: {TEST}")
+    try:
+        result = subprocess.check_output([PROGRAM, TEST], text=True).strip()
+        if result == EXPECT:
+            print(f"Test passed! Result: {result}")
+        else:
+            print(f"Test failed! Result: {result}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error running test: {e}")
+
+# Error tests
+ERROR_TEST = [
+    "2 - -",
+    "2 2 2 -",
+    "test"
+]
+
+for TEST in ERROR_TEST:
+    print(f"Running test: {TEST}")
+    try:
+        subprocess.run([PROGRAM, TEST], check=True, stderr=subprocess.DEVNULL)
+        print("Program did not catch the error.")
+    except subprocess.CalledProcessError:
+        print("Program works as expected.")
+
+print("Testing complete.")
+
+os.system('make fclean')
