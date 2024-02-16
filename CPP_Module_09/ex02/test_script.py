d9ad6a0aa25b570666ce266807327ada4ee4a321
makurz <makurz@student.42heilbronn.de>
2024-02-16 16:58:43 +0100

    update testscript for cpp09/ex02

--- CPP_Module_09/ex02/test_script.py
+++ CPP_Module_09/ex02/test_script.py
@@ -1,6 +1,9 @@
 import subprocess
 import random
+import os
 
+PROGRAM = './PmergeMe'
+MAKE = ['re', 'fclean']
 
 def generate_random_list(size):
     return [str(random.randint(0, 100000000)) for _ in range(size)]
@@ -13,6 +16,15 @@ def run_test(data):
 
 
 if __name__ == '__main__':
+    if not os.system(f"make {MAKE[0]}") == 0:
+        print(f"Error building {PROGRAM}.")
+        exit(1)
+
+    args = ['./PmergeMe'] + [str(x) for x in range(1, 10)]
+    result = subprocess.run(args, capture_output=True, text=True)
+    output = result.stdout.strip()
+    print(f"Test already sorted array:\n{output}\n")
+
     data = generate_random_list(5)
     output = run_test(data)
     print(f"Test with 5 numbers:\n{output}\n")
@@ -25,22 +37,22 @@ if __name__ == '__main__':
     output = run_test(data)
     print(f"Test with 1000 numbers:\n{output}\n")
 
+    data = generate_random_list(1250)
+    output = run_test(data)
+    print(f"Test with 1250 numbers:\n{output}\n")
+
+    data = generate_random_list(2000)
+    output = run_test(data)
+    print(f"Test with 2000 numbers:\n{output}\n")
+
+    data = generate_random_list(2500)
+    output = run_test(data)
+    print(f"Test with 2500 numbers:\n{output}\n")
+
+    data = generate_random_list(3411)
+    output = run_test(data)
+    print(f"Test with 3410 numbers:\n{output}\n")
+
     data = generate_random_list(5000)
     output = run_test(data)
     print(f"Test with 5000 numbers:\n{output}\n")
-
-    data = generate_random_list(10000)
-    output = run_test(data)
-    print(f"Test with 10000 numbers:\n{output}\n")
-
-    data = generate_random_list(25000)
-    output = run_test(data)
-    print(f"Test with 25000 numbers:\n{output}\n")
-
-    data = generate_random_list(50000)
-    output = run_test(data)
-    print(f"Test with 50000 numbers:\n{output}\n")
-
-    data = generate_random_list(100000)
-    output = run_test(data)
-    print(f"Test with 100000 numbers:\n{output}\n")
